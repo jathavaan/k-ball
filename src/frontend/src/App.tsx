a8959aa27f66e2765f51b3809c7aca72d61f97a9
almada <almada@stud.ntnu.no>
2024-10-10 17:00:42 +0900

    feat: #13 created navbar with logo button

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,10 +1,12 @@
+import { Navbar } from "./features/navbar/Navbar.tsx";
 import { PlayerDashboard } from "./pages/PlayerDashboard.tsx";
 import { Container } from "@mui/material";
 
 function App() {
   return (
     <>
-      <Container>
+      <Navbar />
+      <Container style={{ marginTop: "115px" }}>
         <PlayerDashboard />
       </Container>
     </>
