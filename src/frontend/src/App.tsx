dade118b5495ce4b8b039fb42ef3a72dee437e65
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-10-11 10:51:43 +0900

    chore: #26 Created a temporary homepage for the website

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { PlayerDashboard } from "./pages/PlayerDashboard.tsx";
 import { PlayerProfile } from "./pages/PlayerProfile.tsx";
 import { Container } from "@mui/material";
+import { TemporaryHomepage } from "./pages/TemporaryHomepage.tsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Router>
         <Container>
           <Routes>
+            <Route path="/project2" element={<TemporaryHomepage />} />
             <Route path="/project2/players" element={<PlayerDashboard />} />
             <Route
               path="/project2/players/:playerId"
