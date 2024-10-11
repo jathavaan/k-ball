05ab7991cc4ad2d6fd897a0a5bfe2fadafe05c60
almada <almada@stud.ntnu.no>
2024-10-11 10:26:25 +0900

    feat: #13 snapshot tests for Navbar-component and small changes to styling

--- src/frontend/src/features/navbar/navbar.style.ts
+++ src/frontend/src/features/navbar/navbar.style.ts
@@ -2,29 +2,29 @@ import { AppBar, Toolbar, Button, styled } from "@mui/material";
 
 
 export const StyledAppBar= styled(AppBar)(({ theme }) => ({
-    backgroundColor: "#ffffff",
+    backgroundColor: theme.palette.primary.contrastText,
     padding: "0.3rem",
-    position: "fixed",  // Flytter navbaren til fast posisjon på toppen
+    position: "fixed", 
     top: 0,            
     left: 0, 
-    width: "100%",      // Får navbaren til å dekke hele bredden
-    zIndex: theme.zIndex.drawer + 1, // Sørger for at navbaren vises over annet innhold
-    
+    width: "100%",      
+    zIndex: theme.zIndex.drawer + 1,
   }));
 
 export const StyledToolbar= styled(Toolbar)(({ theme }) => ({
-  backgroundColor: "#ffffff",
+  backgroundColor: theme.palette.primary.contrastText,
   padding: "0.3rem",
 }));
 
-export const LogoButton = styled(Button)(({ theme }) => ({
+export const LogoButton = styled(Button)(() => ({
     padding: 0, 
+    
   }));
   
   export const LogoImage = styled("img")({
     height: "60px",
     transition: "transform 0.3s ease", 
-  "&:hover": {
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-    }
+    "&:hover": {
+      boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+      },
   });
