bf4b5235099b81686fcc0ae14dde28dbebbdb4ab
almada <almada@stud.ntnu.no>
2024-10-30 11:43:16 +0900

    fix: #9 added searchbar-component to player dashboard

--- src/frontend/src/pages/player-dashboard/PlayerDashboard.tsx
+++ src/frontend/src/pages/player-dashboard/PlayerDashboard.tsx
@@ -1,11 +1,13 @@
 ﻿import { PlayerCardGrid } from "../../features/player-card";
 import { PlayerFilters } from "../../features/player-filters/PlayerFilters.tsx";
 import Grid from "@mui/material/Grid2";
+import { SearchBar } from "../../features/searchbar/Searchbar.tsx";
 
 export const PlayerDashboard = () => {
   return (
     <Grid container spacing={2}>
       <Grid size={{ xs: 12 }}>
+        <SearchBar />
         <PlayerFilters />
       </Grid>
 
