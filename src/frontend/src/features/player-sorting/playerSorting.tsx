17c8213a6119dec55f43aaf991066f008c815039
almada <almada@stud.ntnu.no>
2024-10-30 12:16:40 +0900

    fix: #8 fixed component file, and deleted useless comments and files

--- src/frontend/src/features/player-sorting/playerSorting.tsx
+++ src/frontend/src/features/player-sorting/playerSorting.tsx
@@ -13,22 +13,6 @@ import {
 } from "./playerSorting.slice.ts";
 
 export const PlayerSorting = () => {
-  /* const [sortBy, setSortBy] = React.useState("name");
-  const [sortOrder, setSortOrder] = React.useState("asc");
-
-  const handleSortChange = (event: SelectChangeEvent) => {
-    const value = event.target.value;
-
-    console.log("Value changed to:", value); 
-
-    const [newSortBy, newSortOrder] = (value as string).split('_') as ["name" | "rating", "asc" | "desc"];
-
-    console.log("New SortBy:", newSortBy);
-    console.log("New SortOrder:", newSortOrder);
-
-    setSortBy(newSortBy);
-    setSortOrder(newSortOrder);
-  }; */
   const dispatch = useDispatch();
   const sortBy = useSelector(selectSortBy);
   const sortOrder = useSelector(selectSortOrder);
@@ -84,21 +68,3 @@ export const PlayerSorting = () => {
     </StyledSortContainer>
   );
 };
-
-{
-  /* return (
-    <StyledSortContainer>
-      <StyledSortSelect
-        value={`${sortBy}_${sortOrder}`}
-        onChange={() => handleSortChange}
-        displayEmpty
-        inputProps={{ "aria-label": "Sort by" }}
-      >
-        <MenuItem value="name_asc">Name (A-Z)</MenuItem>
-        <MenuItem value="name_desc">Name (Z-A)</MenuItem>
-        <MenuItem value="rating_asc">Rating (ASC)</MenuItem>
-        <MenuItem value="rating_desc">Rating (DESC)</MenuItem>
-      </StyledSortSelect>
-    </StyledSortContainer>
-  ); */
-}
