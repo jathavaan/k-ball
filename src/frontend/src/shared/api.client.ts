4edb3a7206273babd712ee229a08e34c4998a48b
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-11-01 19:20:52 +0900

    chore: 65 Fixed bug

--- src/frontend/src/shared/api.client.ts
+++ src/frontend/src/shared/api.client.ts
@@ -1,6 +1,6 @@
 ﻿import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const apiClient = new ApolloClient({
-  uri: "http://localhost:4000/graphql",
+  uri: "http://it2810-25.idi.ntnu.no:4000/graphql",
   cache: new InMemoryCache(),
 });
