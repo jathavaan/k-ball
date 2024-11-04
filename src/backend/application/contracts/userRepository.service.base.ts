87b2d601b42e2149b2ab16906e8aea308e98503a
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-11-04 11:03:02 +0900

    chore: #17 Added password hashing

--- src/backend/application/contracts/userRepository.service.base.ts
+++ src/backend/application/contracts/userRepository.service.base.ts
@@ -4,6 +4,7 @@ export interface UserRepositoryServiceBase {
   getUserById: (id: number) => Promise<User | null>;
   getUserByEmail: (email: string) => Promise<User | null>;
   getUsers: () => Promise<User[]>;
+  checkCredentials: (email: string, password: string) => Promise<number | null>;
   addUser: (user: User) => Promise<boolean>;
   deleteUserById: (id: number) => Promise<boolean>;
 }
