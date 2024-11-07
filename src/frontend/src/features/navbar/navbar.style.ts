b98f18a400a80002ac8053a131f1940cdd2ea75d
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-11-07 09:47:36 +0900

    style: #88 Removed hover on navbar button

--- src/frontend/src/features/navbar/navbar.style.ts
+++ src/frontend/src/features/navbar/navbar.style.ts
@@ -1,4 +1,4 @@
-import { AppBar, Toolbar, Button, styled } from "@mui/material";
+import { AppBar, Button, styled, Toolbar } from "@mui/material";
 
 export const StyledAppBar = styled(AppBar)(({ theme }) => ({
   backgroundColor: theme.palette.primary.contrastText,
@@ -17,12 +17,12 @@ export const StyledToolbar = styled(Toolbar)(({ theme }) => ({
 
 export const LogoButton = styled(Button)(() => ({
   padding: 0,
+  "&:hover": {
+    backgroundColor: "inherit",
+    boxShadow: "none",
+  },
 }));
 
 export const LogoImage = styled("img")({
   height: "60px",
-  transition: "transform 0.3s ease",
-  "&:hover": {
-    boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-  },
 });
