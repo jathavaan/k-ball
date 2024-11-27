61ccd42cad8e27c96088d3708db3f065b225ecce
Jathavaan Shankarr <jathavaan12@gmail.com>
2024-11-27 17:17:21 +0900

    fix: #52 Added serializer to remove hashed value in Mui components

--- src/frontend/src/features/navbar/Navbar.test.tsx
+++ src/frontend/src/features/navbar/Navbar.test.tsx
@@ -1,21 +1,31 @@
 import { render, screen } from "@testing-library/react";
 import { Navbar } from "./Navbar";
 import logo from "../../assets/logo.webp";
+import { MemoryRouter } from "react-router-dom";
 
 describe("Navbar", () => {
+  beforeEach(() => {
+    render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
+  });
   it("should render the logo image", () => {
-    render(<Navbar />);
     expect(screen.getByAltText("Logo")).toBeDefined();
   });
 
   it("Should render the logo image with correct src", () => {
-    render(<Navbar />);
     const logoImage = screen.getByAltText("Logo") as HTMLImageElement;
     expect(logoImage.src).toContain(logo);
   });
 
   it("should match Navbar snapshot", () => {
-    const { asFragment } = render(<Navbar />);
+    const { asFragment } = render(
+      <MemoryRouter>
+        <Navbar />
+      </MemoryRouter>,
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 });
